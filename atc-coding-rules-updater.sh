4cdc6ebd737eb14917a202893469230ffe045ec8
Jan Krag <jankrag@gmail.com>
2021-01-29 14:55:43 +0100

    Remove unnecessary debug output
    Co-authored-by: perkops <43060445+perkops@users.noreply.github.com>

--- atc-coding-rules-updater.sh
+++ atc-coding-rules-updater.sh
@@ -4,7 +4,6 @@ dotnet tool update -g atc-coding-rules-updater
 
 currentPath=$(pwd)
 jsonPath="$currentPath/atc-coding-rules-updater.json"
-echo "$jsonPath"
 
 echo "Run atc-rules-updater to fetch updated rulesets and configurations"
 atc-coding-rules-updater -r "$currentPath" --optionsPath "$jsonPath"  -v true
