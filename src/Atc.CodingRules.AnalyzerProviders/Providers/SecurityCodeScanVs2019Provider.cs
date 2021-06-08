defe3d26acc9a383fb90b89c115ef25ca6a56558
David Kallesen <david@lmdk.dk>
2021-06-08 13:58:43 +0200

    Imp. SecurityCodeScanVs2019Provider

--- src/Atc.CodingRules.AnalyzerProviders/Providers/SecurityCodeScanVs2019Provider.cs
+++ src/Atc.CodingRules.AnalyzerProviders/Providers/SecurityCodeScanVs2019Provider.cs
@@ -1,14 +1,44 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Atc.CodingRules.AnalyzerProviders.Models;
+using HtmlAgilityPack;
 
 namespace Atc.CodingRules.AnalyzerProviders.Providers
 {
     public class SecurityCodeScanVs2019Provider : AnalyzerProviderBase
     {
+        [SuppressMessage("Minor Code Smell", "S1075:URIs should not be hardcoded", Justification = "OK.")]
+        public override Uri? DocumentationLink { get; set; } = new Uri("https://security-code-scan.github.io", UriKind.Absolute);
+
         public override AnalyzerProviderBaseRuleData CollectBaseRules()
         {
             var data = new AnalyzerProviderBaseRuleData("SecurityCodeScan.VS2019");
 
-            // TODO: Fix this..
+            var web = new HtmlWeb();
+            var htmlDoc = web.Load(DocumentationLink);
+            var headers3 = htmlDoc.DocumentNode.SelectNodes("//h3").ToList();
+
+            foreach (var item in headers3)
+            {
+                if (!item.InnerText.Contains("SCS", StringComparison.Ordinal))
+                {
+                    continue;
+                }
+
+                var sa = item.InnerText.Split(" - ");
+
+                var code = sa[0];
+                var title = sa[1];
+                var link = this.DocumentationLink!.OriginalString + $"#{code.ToLower(GlobalizationConstants.EnglishCultureInfo)}---{title.ToLower(GlobalizationConstants.EnglishCultureInfo).Replace(" ", "-", StringComparison.Ordinal)}";
+
+                data.Rules.Add(
+                    new Rule(
+                        code,
+                        title,
+                        link));
+            }
+
             return data;
         }
     }
