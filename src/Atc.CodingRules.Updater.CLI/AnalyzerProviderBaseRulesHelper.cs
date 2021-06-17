adc3de061ceaf642a031e6a25d5ba7cabb9e00cf
David Kallesen <david@lmdk.dk>
2021-06-17 13:35:19 +0200

    Improve handling of section "ATC autogenerated suppressions generated"

--- src/Atc.CodingRules.Updater.CLI/AnalyzerProviderBaseRulesHelper.cs
+++ src/Atc.CodingRules.Updater.CLI/AnalyzerProviderBaseRulesHelper.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
@@ -10,10 +11,13 @@ using System.Threading.Tasks;
 using Atc.CodingRules.AnalyzerProviders;
 using Atc.CodingRules.AnalyzerProviders.Models;
 
+// ReSharper disable InvertIf
 namespace Atc.CodingRules.Updater.CLI
 {
     public static class AnalyzerProviderBaseRulesHelper
     {
+        private const string AtcAnalyzerProviderBaseRulesFileName = "AtcAnalyzerProviderBaseRules.json";
+
         public static async Task<Collection<AnalyzerProviderBaseRuleData>> GetAnalyzerProviderBaseRules(CancellationToken cancellationToken = default)
         {
             var jsonOptions = new JsonSerializerOptions
@@ -23,9 +27,8 @@ namespace Atc.CodingRules.Updater.CLI
                 DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
             };
 
-            var tempFile = Path.Combine(Path.GetTempPath(), "AtcAnalyzerProviderBaseRules.json");
+            var tempFile = Path.Combine(Path.GetTempPath(), AtcAnalyzerProviderBaseRulesFileName);
             var fileInfo = new FileInfo(tempFile);
-
             if (fileInfo.Exists && fileInfo.LastWriteTimeUtc > DateTime.UtcNow.AddDays(-1))
             {
                 var fileAsJson = await File.ReadAllTextAsync(tempFile, cancellationToken);
@@ -35,8 +38,12 @@ namespace Atc.CodingRules.Updater.CLI
             var analyzerProviders = new AnalyzerProviderCollector();
             var analyzerProviderBaseRules = await analyzerProviders.CollectAllBaseRules(cancellationToken);
 
-            var json = JsonSerializer.Serialize(analyzerProviderBaseRules, jsonOptions);
-            await File.WriteAllTextAsync(tempFile, json, Encoding.UTF8, cancellationToken);
+            bool hasErrors = analyzerProviderBaseRules.Any(x => !string.IsNullOrEmpty(x.ExceptionMessage));
+            if (!hasErrors)
+            {
+                var json = JsonSerializer.Serialize(analyzerProviderBaseRules, jsonOptions);
+                await File.WriteAllTextAsync(tempFile, json, Encoding.UTF8, cancellationToken);
+            }
 
             return analyzerProviderBaseRules;
         }
