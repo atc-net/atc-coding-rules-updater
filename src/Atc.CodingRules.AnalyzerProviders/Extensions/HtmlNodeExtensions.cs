2af414e3348acff46260b4bde4a88b119236c91f
David Kallesen <david@lmdk.dk>
2021-07-06 01:45:03 +0200

    Added HtmlNodeExtensions with HasTitleWithAccessDenied

---
+++ src/Atc.CodingRules.AnalyzerProviders/Extensions/HtmlNodeExtensions.cs
@@ -0,0 +1,21 @@
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace HtmlAgilityPack
+{
+    public static class HtmlNodeExtensions
+    {
+        public static bool HasTitleWithAccessDenied(this HtmlNode hmlNode)
+        {
+            if (hmlNode == null)
+            {
+                throw new ArgumentNullException(nameof(hmlNode));
+            }
+
+            var titleText = hmlNode.SelectSingleNode("//*//title")?.InnerText;
+            return titleText is not null &&
+                   titleText.Contains("Access", StringComparison.OrdinalIgnoreCase) &&
+                   titleText.Contains("Denied", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
