c6bfb378db4075c8b7207cef64cefe4123e7cd81
David Kallesen <david@lmdk.dk>
2022-02-01 10:15:08 +0100

    Improve method GetCurrentVersion to actuall get the .exe file file-version

--- src/Atc.CodingRules.Updater/CodingRulesUpdaterVersionHelper.cs
+++ src/Atc.CodingRules.Updater/CodingRulesUpdaterVersionHelper.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Reflection;
 
 namespace Atc.CodingRules.Updater;
@@ -6,10 +7,16 @@ public static class CodingRulesUpdaterVersionHelper
 {
     public static Version GetCurrentVersion()
     {
-        var assembly = Assembly.GetAssembly(typeof(CodingRulesUpdaterVersionHelper));
-        return assembly is null
-            ? new Version(1, 0, 0, 0)
-            : assembly.GetName().Version ?? new Version(1, 0, 0, 0);
+        var assembly = Assembly.GetExecutingAssembly();
+        var fileVersion = FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
+        if (fileVersion is null)
+        {
+            return new Version(1, 0, 0, 0);
+        }
+
+        return Version.TryParse(fileVersion, out var version)
+            ? version
+            : new Version(1, 0, 0, 0);
     }
 
     public static Version? GetLatestVersion()
