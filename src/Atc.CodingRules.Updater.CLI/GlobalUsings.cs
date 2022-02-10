7decfd74f54b0a8723616ca82376f70b3b2168cc
Per Kops <pko@delegate.dk>
2022-02-10 12:16:35 +0100

    Cleanup GlobalUsings in CLI tool

--- src/Atc.CodingRules.Updater.CLI/GlobalUsings.cs
+++ src/Atc.CodingRules.Updater.CLI/GlobalUsings.cs
@@ -1,15 +1,11 @@
 global using System;
-global using System.Collections.Generic;
 global using System.Collections.ObjectModel;
 global using System.Data;
 global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
-global using System.IO;
-global using System.Linq;
 global using System.Text;
 global using System.Text.Json;
-global using System.Threading.Tasks;
 
 global using Atc.CodingRules.AnalyzerProviders;
 global using Atc.CodingRules.AnalyzerProviders.Models;
