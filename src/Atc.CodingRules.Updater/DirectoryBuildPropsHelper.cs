a713229fc7073d04b1f6e6c299775e07a7bb02e5
David Kallesen <david@lmdk.dk>
2022-04-01 00:48:00 +0200

    fix: Use the new method IsNewerMinorReleaseThen from Atc #5aec3431b034ab16e9ad9c4690be8e440e449ff6
    …4ab16e9ad9c4690be8e440e449ff6

--- src/Atc.CodingRules.Updater/DirectoryBuildPropsHelper.cs
+++ src/Atc.CodingRules.Updater/DirectoryBuildPropsHelper.cs
@@ -198,7 +198,7 @@ public static class DirectoryBuildPropsHelper
                     var latestVersion = AtcApiNugetClientHelper.GetLatestVersionForPackageId(logger, item.PackageId, CancellationToken.None);
 
                     if (latestVersion is not null &&
-                        latestVersion.GreaterThan(version, significantParts: 4, startingPart: 2))
+                        latestVersion.IsNewerMinorReleaseThen(version))
                     {
                         result.Add(
                             new DotnetNugetPackage(
