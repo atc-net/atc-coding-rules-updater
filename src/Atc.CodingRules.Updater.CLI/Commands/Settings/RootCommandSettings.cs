05d0dccb621a95fa9ec5b7fc4e043fc6af319e93
Per Kops <pko@delegate.dk>
2024-04-02 11:56:24 +0200

    refact: change RootCommandSettings to utilize new shortversion of version aswell as long-version

--- src/Atc.CodingRules.Updater.CLI/Commands/Settings/RootCommandSettings.cs
+++ src/Atc.CodingRules.Updater.CLI/Commands/Settings/RootCommandSettings.cs
@@ -2,7 +2,7 @@ namespace Atc.CodingRules.Updater.CLI.Commands.Settings;
 
 public class RootCommandSettings : BaseCommandSettings
 {
-    [CommandOption(CommandConstants.ArgumentLongVersion)]
+    [CommandOption($"{CommandConstants.ArgumentShortVersion}|{CommandConstants.ArgumentLongVersion}")]
     [Description("Display version")]
     public bool? Version { get; init; }
 }
