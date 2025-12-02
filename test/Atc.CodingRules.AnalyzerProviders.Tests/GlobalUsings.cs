1dd719a1fedd84e0fd00d987e95876b06c59c1c7
Per Kops <pko@delegate.dk>
2025-12-02 09:24:34 +0100

    test: update tests to work with latest xunit version

--- test/Atc.CodingRules.AnalyzerProviders.Tests/GlobalUsings.cs
+++ test/Atc.CodingRules.AnalyzerProviders.Tests/GlobalUsings.cs
@@ -1,6 +1,2 @@
 global using Atc.CodingRules.AnalyzerProviders.Providers;
-global using Atc.XUnit;
-
 global using Microsoft.Extensions.Logging.Abstractions;
-
-global using Xunit;
--- test/Atc.CodingRules.Updater.Tests/GlobalUsings.cs
+++ test/Atc.CodingRules.Updater.Tests/GlobalUsings.cs
@@ -1,2 +1,1 @@
 global using System.Reflection;
-global using Xunit.Abstractions;
