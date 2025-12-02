dce54b457e706fc646df0e927af51fcf509786e6
Per Kops <pko@delegate.dk>
2025-12-02 11:35:56 +0100

    fix(providers): update MicrosoftVisualStudioThreadingAnalyzersProvider URL
    - Update DocumentationLink to correct analyzer index page
    - Fix href link construction for relative URLs

--- test/Atc.CodingRules.AnalyzerProviders.Tests/GlobalUsings.cs
+++ test/Atc.CodingRules.AnalyzerProviders.Tests/GlobalUsings.cs
@@ -1,2 +1,4 @@
+global using System.Diagnostics.CodeAnalysis;
+global using Atc.CodingRules.AnalyzerProviders.Models;
 global using Atc.CodingRules.AnalyzerProviders.Providers;
 global using Microsoft.Extensions.Logging.Abstractions;
