b73729510f00a6bced8e64ddaaf5c2c3f2a5a9ab
davidkallesen <david@lmdk.dk>
2026-01-05 22:04:45 +0100

    fix: add missing Directory.Build.props for DotNet10 projects

--- src/Atc.CodingRules.Updater.CLI/ProjectHelper.cs
+++ src/Atc.CodingRules.Updater.CLI/ProjectHelper.cs
@@ -34,7 +34,8 @@ public static class ProjectHelper
             or SupportedProjectTargetType.DotNet6
             or SupportedProjectTargetType.DotNet7
             or SupportedProjectTargetType.DotNet8
-            or SupportedProjectTargetType.DotNet9)
+            or SupportedProjectTargetType.DotNet9
+            or SupportedProjectTargetType.DotNet10)
         {
             HandleDirectoryBuildPropsFiles(logger, projectPath, options);
 
